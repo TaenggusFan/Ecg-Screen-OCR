6f3582202361c3fed630510b8c7559c8f8c053a2
TaenggusFan <132985698+TaenggusFan@users.noreply.github.com>
2023-11-21 15:27:28 +0800

    專案Python檔

--- Python/s2_3_pic_process.py
+++ Python/s2_3_pic_process.py
@@ -1,3 +1,4 @@
+#圖片灰階處裡 針對圖片性質愈線處理或物件偵測裁剪後再處裡
 import cv2
 import numpy as np
 import glob
@@ -9,14 +10,13 @@ dirs = glob.glob(image_path + './*jpg')
 def Gray(image_path):
     for i in range(len(dirs)):
         image = cv2.imread(dirs[i])
-        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray = cv2.cvtColor(image, 7)
-        invert = 255 - gray
-        # gray2 = cv2.adaptiveThreshold(invert, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 7)
-        # invert = 255 - thresh
-        # ImgN = ".\\gray\\" + "gray" + str(i) + ".jpeg"
-        # cv2.imwrite(ImgN, invert)
-        cv2.imshow('oxxostudio', invert)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #灰階方式1
+        #gray = cv2.cvtColor(image, 7) #灰階方式2
+        #invert = 255 - gray 
+        gray2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 7)
+        #ImgN = ".\\Pic_gray\\" + "gray" + str(i) + ".jpg"
+        #cv2.imwrite(ImgN, invert)
+        cv2.imshow('oxxostudio', gray2)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
